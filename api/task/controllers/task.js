cb731a73beb7397e18b387a8ee793a5e84aacf06
Đào Tuấn Anh <anhdt.developer@gmail.com>
2021-01-14 22:41:33 +0700

    update statistic query

--- api/task/controllers/task.js
+++ api/task/controllers/task.js
@@ -1,8 +1,36 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-
-module.exports = {};
+module.exports = {
+  async statistic(ctx) {
+    return await strapi.connections.default.raw(
+      `SELECT
+      u.id,
+      u.title,
+      COUNT (t.id) total,
+      SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'waiting') then 1 else 0 end) waiting,
+      SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'todo' ) then 1 else 0 end) todo,
+      SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'doing' ) then 1 else 0 end) doing,
+      SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'aprroving' ) then 1 else 0 end) aprroving,
+      SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'recovered' ) then 1 else 0 end) recovered,
+      SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'done' ) then 1 else 0 end) done,
+  
+      SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'waiting') then 1 else 0 end) waitingOutDate,
+      SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'todo' ) then 1 else 0 end) todoOutDate,
+      SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'doing' ) then 1 else 0 end) doingOutDate,
+      SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'aprroving' ) then 1 else 0 end) aprrovingOutDate,
+      SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'recovered' ) then 1 else 0 end) recoveredOutDate,
+      SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'done' ) then 1 else 0 end) doneOutDate
+  FROM
+      dbo.tasks t
+      INNER JOIN dbo.units u
+      ON u.id = t.executedUnit
+  GROUP BY
+      u.id,
+      u.title`
+    );
+  },
+};
