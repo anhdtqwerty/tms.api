e6757db8575a9cdf4d4e65d9a947e24633aa9ccf
Đào Tuấn Anh <anhdt.developer@gmail.com>
2021-01-14 23:38:05 +0700

    updatre public quere

--- api/task/controllers/task.js
+++ api/task/controllers/task.js
@@ -14,20 +14,22 @@ module.exports = {
       SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'waiting') then 1 else 0 end) waiting,
       SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'todo' ) then 1 else 0 end) todo,
       SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'doing' ) then 1 else 0 end) doing,
-      SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'aprroving' ) then 1 else 0 end) aprroving,
       SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'recovered' ) then 1 else 0 end) recovered,
-      SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'done' ) then 1 else 0 end) done,
+      SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'done' AND t.status = 'approving' ) then 1 else 0 end) approving,
+      SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'done' AND t.status = 'approved' ) then 1 else 0 end) done,
   
       SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'waiting') then 1 else 0 end) waitingOutDate,
       SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'todo' ) then 1 else 0 end) todoOutDate,
       SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'doing' ) then 1 else 0 end) doingOutDate,
-      SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'aprroving' ) then 1 else 0 end) aprrovingOutDate,
       SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'recovered' ) then 1 else 0 end) recoveredOutDate,
-      SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'done' ) then 1 else 0 end) doneOutDate
+      SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'done' AND t.status = 'approving') then 1 else 0 end) approvingOutDate,
+      SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'done' AND t.status = 'approved') then 1 else 0 end) doneOutDate
   FROM
       dbo.tasks t
       INNER JOIN dbo.units u
       ON u.id = t.executedUnit
+  WHERE
+      t.created_at BETWEEN '${ctx.query.from}' AND '${ctx.query.to}'
   GROUP BY
       u.id,
       u.title`
