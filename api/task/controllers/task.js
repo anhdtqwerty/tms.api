c496f3b3b3f7345dd0b7877d2fd926c3340e2e96
Đào Tuấn Anh <anhdt.developer@gmail.com>
2021-04-13 20:42:26 +0700

    update

--- api/task/controllers/task.js
+++ api/task/controllers/task.js
@@ -41,7 +41,7 @@ module.exports = {
       SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'recovered' ) then 1 else 0 end) recoveredOutDate,
       SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'returned' ) then 1 else 0 end) returnedOutDate,
       SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'done' AND t.status = 'approving') then 1 else 0 end) approvingOutDate,
-      SUM(CASE WHEN (t.expiredDate < t.doneDate) AND t.state = 'done' AND t.status = 'approved') then 1 else 0 end) doneOutDate
+      SUM(CASE WHEN (t.expiredDate < t.doneDate AND t.state = 'done' AND t.status = 'approved') then 1 else 0 end) doneOutDate
   FROM
       dbo.tasks t
       INNER JOIN dbo.units u
@@ -89,7 +89,7 @@ module.exports = {
     SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'recovered' ) then 1 else 0 end) recoveredOutDate,
     SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'returned' ) then 1 else 0 end) returnedOutDate,
     SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'done' AND t.status = 'approving') then 1 else 0 end) approvingOutDate,
-    SUM(CASE WHEN (t.expiredDate < t.doneDate) AND t.state = 'done' AND t.status = 'approved') then 1 else 0 end) doneOutDate
+    SUM(CASE WHEN (t.expiredDate < t.doneDate AND t.state = 'done' AND t.status = 'approved') then 1 else 0 end) doneOutDate
     FROM
         dbo.tasks t
         INNER JOIN dbo.departments d
@@ -127,7 +127,7 @@ module.exports = {
     SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'recovered' ) then 1 else 0 end) recovered,
     SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'returned' ) then 1 else 0 end) returned,
     SUM(CASE WHEN (t.expiredDate > CONVERT(date, GETDATE()) AND t.state = 'done' AND t.status = 'approving' ) then 1 else 0 end) approving,
-    SUM(CASE WHEN (t.expiredDate > t.doneDate) AND t.state = 'done' AND t.status = 'approved' ) then 1 else 0 end) done,
+    SUM(CASE WHEN (t.expiredDate > t.doneDate AND t.state = 'done' AND t.status = 'approved' ) then 1 else 0 end) done,
 
     SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'waiting') then 1 else 0 end) waitingOutDate,
     SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'todo' ) then 1 else 0 end) todoOutDate,
@@ -135,7 +135,7 @@ module.exports = {
     SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'recovered' ) then 1 else 0 end) recoveredOutDate,
     SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'returned' ) then 1 else 0 end) returnedOutDate,
     SUM(CASE WHEN (t.expiredDate < CONVERT(date, GETDATE()) AND t.state = 'done' AND t.status = 'approving') then 1 else 0 end) approvingOutDate,
-    SUM(CASE WHEN (t.expiredDate < t.doneDate) AND t.state = 'done' AND t.status = 'approved') then 1 else 0 end) doneOutDate
+    SUM(CASE WHEN (t.expiredDate < t.doneDate AND t.state = 'done' AND t.status = 'approved') then 1 else 0 end) doneOutDate
     FROM
         dbo.tasks t
         INNER JOIN dbo.comrades c
